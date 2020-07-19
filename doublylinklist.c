616dd4b914dc6bfcbe54ec01776dd646841b23d5
shreya-n-kumari <68438422+shreya-n-kumari@users.noreply.github.com>
2020-07-19 22:44:52 +0530

    updated doublylinklist

--- doublylinklist.c
+++ doublylinklist.c
@@ -11,6 +11,11 @@ void InsertEnd(struct dll **);
 void display(struct dll **);
 void InsertMiddle(struct dll **, int);
 int countNode(struct dll **);
+void InsertPosition(struct dll **);
+void deleteFirst(struct dll **);
+void deleteLast(struct dll **);
+void deleteMiddle(struct dll **, int);
+void deletePosition(struct dll **);
 int main()
 {
     struct dll *head=NULL;
@@ -28,6 +33,11 @@ int main()
         printf("\n4.Enter node at the middle of list:");
         printf("\n5.count the node:");
         printf("\n6.exit from the list");
+        printf("\n7.Enter node at any position:");
+        printf("\n8.Delete First Node of The List");
+        printf("\n9.Delete last node");
+        printf("\n10.Delete middle node");
+        printf("\n11.Delete node at any position");
         printf("\n................................");
         printf("\nenter your choice:");
         scanf("%d",&choice);
@@ -53,6 +63,22 @@ int main()
             case 6:
                 exit(0);
                 break;
+            case 7:
+                InsertPosition(&head);
+                break;
+            case 8:
+                deleteFirst(&head);
+                break;
+            case 9:
+                deleteLast(&head);
+                break;
+            case 10:
+                nodeCount = countNode(&head);
+                deleteMiddle(&head, nodeCount);
+                break;
+            case 11:
+                deletePosition(&head);
+                break;  
             default:
             printf("\nyou have entered invalid choice");
     }
@@ -155,4 +181,114 @@ void InsertMiddle(struct dll **head,int n)
     t->rlink=newNode;
     newNode->llink=t;
     newNode->rlink->llink=newNode;
+}
+//............................INSERT NODE AT ANY POSITION..............
+void InsertPosition(struct dll **head)
+{
+    struct dll *newNode=(struct dll *)malloc(sizeof(struct dll));
+    int n;
+    printf("\nenter number of position");
+    scanf("%d",&n);
+    n=n-1;
+    printf("enter ID:");
+    scanf("%d",&newNode->id);
+    newNode->rlink=NULL;
+    newNode->llink=NULL;
+    struct dll *t=*head;
+    while(n!=0)
+    {
+        t=t->rlink;
+        n--;
+    }
+    newNode->llink=t;
+    newNode->rlink=t->rlink;
+    t->rlink=newNode;
+    t->rlink->llink=newNode;
+}   
+//.......................DELETE FIRST NODE......................
+void deleteFirst(struct dll **head)
+{
+    if(*head==NULL){
+    printf("list is empty");
+    }
+    else{
+        printf("%d",(*head)->id);
+        *head=(*head)->rlink;
+        (*head)->llink->rlink=NULL;
+        (*head)->llink=NULL;
+    }
+}
+//...................DELETE LAST NODE........................
+void deleteLast(struct dll **head)
+{
+    struct dll *t=*head;
+    if(*head==NULL)
+    {
+        printf("\nList is empty");
+    }
+    else{
+         while(t->rlink!=NULL)
+         {
+             t=t->rlink;
+         }
+         t->llink->rlink=NULL;
+         t->llink=NULL;
+         printf("\nID = %d",t->id);
+         free(t);
+    }
+}
+//.........................DELETE MIDDLE NODE....................
+void deleteMiddle(struct dll **head,int count)
+{
+    struct dll *t=*head;
+    struct dll *p=NULL;
+    int mid=count/2;
+    mid=mid-1;
+    if(*head==NULL)
+    {
+        printf("List is empty");
+    }
+    else{
+       
+        while(mid!=0)
+        {
+            p=t;
+            t=t->rlink;
+            mid--;
+        }
+        p->rlink=t->rlink;
+        t->rlink->llink=p;
+        t->rlink=NULL;
+        t->llink=NULL;
+        printf("\nID = %d",t->id);
+        free(t);
+    }
+}
+void deletePosition(struct dll ** head)
+{
+    struct dll *t=*head;
+    struct dll *p=NULL;
+    int n;
+    printf("\nEnter node number to delete.");
+    scanf("%d",&n);
+    n=n-1;
+    if(*head==NULL)
+    {
+        printf("List is empty");
+    }
+    
+    else{
+        while(n!=0)
+        {
+            p=t;
+            t=t->rlink;
+            n--;
+        }
+        p->rlink=t->rlink;
+        t->rlink->llink=p;
+        t->rlink=NULL;
+        t->llink=NULL;
+        printf("\nID = %d",t->id);
+        free(t);
+    }
 }
